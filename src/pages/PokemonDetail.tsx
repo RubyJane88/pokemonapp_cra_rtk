ab3aff0e7057c1543d0768a119d34ef911f07772
RJ Cabagnot <rjcabagnot@hotmail.com>
2021-04-26 23:33:25 +0200

    updated

--- src/pages/PokemonDetail.tsx
+++ src/pages/PokemonDetail.tsx
@@ -28,140 +28,142 @@ const PokemonDetail = () => {
         display: "grid",
       }}
     >
-      <BorderBox>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-            flexDirection: "row",
-            alignItems: "center",
-            padding: "3rem",
-          }}
-        >
-          <Link
+      <div style={{ margin: "auto 10rem" }}>
+        <BorderBox>
+          <div
             style={{
-              backgroundColor: "darkorange",
-              padding: "1rem",
-              borderRadius: "5px",
-              color: "white",
+              display: "flex",
+              justifyContent: "space-around",
+              flexDirection: "row",
+              alignItems: "center",
+              padding: "3rem",
             }}
-            to={"/poke-details/" + (parseInt(id) - 1)}
           >
-            Back
-          </Link>
-        </div>
-
-        <div style={{ justifyItems: "center", display: "grid" }}>
-          <H1>{pokemonDetail?.name} </H1>
-        </div>
-
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-            flexDirection: "row",
-            alignItems: "center",
-            padding: "3rem",
-          }}
-        >
-          <Link
-            style={{
-              backgroundColor: "darkolivegreen",
-              color: "white",
-              padding: "1rem",
-              margin: "2rem",
-              borderRadius: "5px",
-            }}
-            to={"/poke-details/" + (parseInt(id) + 1)}
-          >
-            Next
-          </Link>
-        </div>
-
-        <div
-          style={{
-            display: "grid",
-            background: "lightgray",
-            gridTemplateColumns: "30rem 30rem",
-            gridTemplateRows: "30rem 30rem",
-            margin: "2rem",
-            padding: "2rem",
-          }}
-        >
-          <Card>
-            {/*if id is true = render img  */}
-
-            <div
+            <Link
               style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-                height: "100%",
+                backgroundColor: "darkorange",
+                padding: "1rem",
+                borderRadius: "5px",
+                color: "white",
               }}
+              to={"/poke-details/" + (parseInt(id) - 1)}
             >
-              {id && (
-                <img
-                  height={"80%"}
-                  width={"80%"}
-                  src={`https://pokeres.bastionbot.org/images/pokemon/${id}.png`}
-                  alt="picture"
-                />
-              )}
-            </div>
-          </Card>
+              Back
+            </Link>
+          </div>
+
+          <div style={{ justifyItems: "center", display: "grid" }}>
+            <H1>{pokemonDetail?.name} </H1>
+          </div>
 
           <div
             style={{
-              gridTemplateRows: "20rem 10rem 20rem",
-              gridTemplateColumns: "20rem 10rem 20rem",
+              display: "flex",
+              justifyContent: "space-around",
+              flexDirection: "row",
+              alignItems: "center",
+              padding: "3rem",
+            }}
+          >
+            <Link
+              style={{
+                backgroundColor: "darkolivegreen",
+                color: "white",
+                padding: "1rem",
+                margin: "2rem",
+                borderRadius: "5px",
+              }}
+              to={"/poke-details/" + (parseInt(id) + 1)}
+            >
+              Next
+            </Link>
+          </div>
+
+          <div
+            style={{
+              display: "grid",
+              background: "lightgray",
+              gridTemplateColumns: "30rem 30rem",
+              gridTemplateRows: "30rem 30rem",
+              margin: "2rem",
               padding: "2rem",
             }}
           >
-            <P>
-              Pokémon is short for “Pocket Monsters", the original Japanese
-              name. The franchise has its roots in a gaming magazine in the
-              early 1980s in Japan—Game Freak, started by Satoshi Tajiri and Ken
-              Sugimori.
-            </P>
+            <Card>
+              {/*if id is true = render img  */}
 
-            <div>
-              <P>Base Experience: </P>
-              {pokemonDetail?.base_experience}
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
+                {id && (
+                  <img
+                    height={"80%"}
+                    width={"80%"}
+                    src={`https://pokeres.bastionbot.org/images/pokemon/${id}.png`}
+                    alt="picture"
+                  />
+                )}
+              </div>
+            </Card>
+
+            <div
+              style={{
+                gridTemplateRows: "20rem 10rem 20rem",
+                gridTemplateColumns: "20rem 10rem 20rem",
+                padding: "2rem",
+              }}
+            >
+              <P>
+                Pokémon is short for “Pocket Monsters", the original Japanese
+                name. The franchise has its roots in a gaming magazine in the
+                early 1980s in Japan—Game Freak, started by Satoshi Tajiri and
+                Ken Sugimori.
+              </P>
+
+              <div>
+                <P>Base Experience: </P>
+                {pokemonDetail?.base_experience}
+              </div>
+
+              <div>
+                <h2> Abilities</h2>
+                {pokemonDetail?.abilities?.map((ability) => (
+                  <div key={ability.ability.name}> {ability.ability.name} </div>
+                ))}
+              </div>
+
+              <div>
+                <h2> Height</h2>
+                {pokemonDetail?.height}
+              </div>
             </div>
 
-            <div>
-              <h2> Abilities</h2>
-              {pokemonDetail?.abilities?.map((ability) => (
-                <div key={ability.ability.name}> {ability.ability.name} </div>
+            <div style={{ marginLeft: "3rem" }}>
+              <H1> Stats</H1>
+              <div>
+                {pokemonDetail?.stats?.map((stat) => (
+                  <li key={stat.stat.name}>
+                    {` ${stat.stat.name}  ${stat.base_stat}  ${stat.effort} `}
+                  </li>
+                ))}
+              </div>
+            </div>
+
+            <div style={{ marginLeft: "2rem" }}>
+              <H1>Moves </H1>
+              {pokemonDetail?.moves?.map((v) => (
+                <li key={v.move.url}>{v.move.name}, </li>
               ))}
             </div>
-
-            <div>
-              <h2> Height</h2>
-              {pokemonDetail?.height}
-            </div>
           </div>
-
-          <div style={{ marginLeft: "3rem" }}>
-            <H1> Stats</H1>
-            <div>
-              {pokemonDetail?.stats.map((stat) => (
-                <li key={stat.stat.name}>
-                  {` ${stat.stat.name}  ${stat.base_stat}  ${stat.effort} `}
-                </li>
-              ))}
-            </div>
-          </div>
-
-          <div style={{ marginLeft: "2rem" }}>
-            <H1>Moves </H1>
-            {pokemonDetail?.moves?.map((v) => (
-              <li key={v.move.url}>{v.move.name}, </li>
-            ))}
-          </div>
-        </div>
-      </BorderBox>
+        </BorderBox>{" "}
+      </div>
     </div>
   );
 };
